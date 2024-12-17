0243f5bf85ce394949cd63020ab5cfe785370f4f
David Zhu <davidyzhu2@gmail.com>
2024-12-17 12:15:26 -0800

    Add styling to buttons

--- src/index.js
+++ src/index.js
@@ -2,13 +2,47 @@ import "./styles.css"
 import homePage from "./home.js";
 import menuPage from "./menu.js";
 import aboutPage from "./about.js";
+import dripping from "./images/dripping.png";
 
 const homeButton = document.querySelector(".home");
 const menuButton = document.querySelector(".menu");
 const aboutButton = document.querySelector(".about");
+const drip = document.createElement("img");
+drip.setAttribute("src", dripping);
+drip.classList.add("dripping");
 
 homePage();
 
-homeButton.addEventListener("click", homePage);
-menuButton.addEventListener("click", menuPage);
-aboutButton.addEventListener("click", aboutPage);
+function resetStyles () {
+    [homeButton, menuButton, aboutButton].forEach(e => {
+        if (e.classList.contains("cover")) {
+            e.classList.remove("cover");
+        }
+        const images = document.querySelectorAll("button > img");
+        images.forEach(e => {
+            e.remove();
+        })
+    })
+}
+
+
+homeButton.addEventListener("click", () => {
+    resetStyles();
+    homePage();
+    homeButton.classList.add("cover");
+    homeButton.appendChild(drip);
+});
+
+menuButton.addEventListener("click", () => {
+    resetStyles();
+    menuPage();
+    menuButton.classList.add("cover");
+    menuButton.appendChild(drip);
+});
+
+aboutButton.addEventListener("click", () => {
+    resetStyles();
+    aboutPage();
+    aboutButton.classList.add("cover");
+    aboutButton.appendChild(drip);
+});
